bfd82faf381ac244273f013580b767d37afe4733
Michael Pilquist <mpilquist@gmail.com>
2014-10-28 23:54:50 -0400

    Partial implementation of unsigned byte, short, and int

--- src/main/scala/scodec/interop/spire/package.scala
+++ src/main/scala/scodec/interop/spire/package.scala
@@ -3,7 +3,7 @@ package interop
 
 import scalaz.\/
 import \/.{ left, right }
-import _root_.spire.math.Interval
+import _root_.spire.math.{ Interval, UByte, UInt, UShort, ULong }
 
 /**
  * Integrates Spire with scodec.
@@ -12,6 +12,11 @@ package object spire {
 
   /** Provides enrichments for `Codec[A]` related to Spire. */
   implicit class SpireCodecEnrichment[A](val codec: Codec[A]) extends AnyVal {
+
+    def toUByte(implicit ev: A =:= Byte): Codec[UByte] = codec.xmap[UByte](b => UByte(b), _.toByte.asInstanceOf[A]).withToString(codec.toString)
+    def toUShort(implicit ev: A =:= Short): Codec[UShort] = codec.xmap[UShort](s => UShort(s), _.toShort.asInstanceOf[A]).withToString(codec.toString)
+    def toUInt(implicit ev: A =:= Int): Codec[UInt] = codec.xmap[UInt](i => UInt(i), _.toInt.asInstanceOf[A]).withToString(codec.toString)
+    def toULong(implicit ev: A =:= Long): Codec[ULong] = codec.xmap[ULong](l => ULong(l), _.toLong.asInstanceOf[A]).withToString(codec.toString)
 
     /**
      * Returns a codec that validates encoded and decoded values are contained
@@ -28,4 +33,40 @@ package object spire {
     }
   }
 
+  val ubyte: Codec[UByte] = codecs.byte.toUByte.withToString("8-bit unsigned byte")
+
+  val ushort8: Codec[UShort] = codecs.ushort8.toUShort
+  val ushort16: Codec[UShort] = codecs.short16.toUShort.withToString("16-bit unsigned short")
+  def ushort(bits: Int): Codec[UShort] = {
+    require(bits > 0 && bits <= 16)
+    if (bits < 16) codecs.ushort(bits).toUShort.withToString(s"$bits-bit unsigned short")
+    else ushort16
+  }
+
+  val ushort16L: Codec[UShort] = codecs.short16L.toUShort.withToString("16-bit unsigned short")
+  def ushortL(bits: Int): Codec[UShort] = {
+    require(bits > 0 && bits <= 16)
+    if (bits < 16) codecs.ushortL(bits).toUShort.withToString(s"$bits-bit unsigned short")
+    else ushort16L
+  }
+
+  val uint8: Codec[UInt] = codecs.uint8.toUInt
+  val uint16: Codec[UInt] = codecs.uint16.toUInt
+  val uint24: Codec[UInt] = codecs.uint24.toUInt
+  val uint32: Codec[UInt] = codecs.int32.toUInt.withToString("32-bit unsigned integer")
+  def uint(bits: Int): Codec[UInt] = {
+    require(bits > 0 && bits <= 32)
+    if (bits < 32) codecs.uint(bits).toUInt
+    else uint32
+  }
+
+  val uint8L: Codec[UInt] = codecs.uint8L.toUInt
+  val uint16L: Codec[UInt] = codecs.uint16L.toUInt
+  val uint24L: Codec[UInt] = codecs.uint24L.toUInt
+  val uint32L: Codec[UInt] = codecs.int32L.toUInt.withToString("32-bit unsigned integer")
+  def uintL(bits: Int): Codec[UInt] = {
+    require(bits > 0 && bits <= 32)
+    if (bits < 32) codecs.uintL(bits).toUInt
+    else uint32L
+  }
 }
