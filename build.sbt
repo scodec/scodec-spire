452aa39f46cfa0b273c736c3a4b128ea720dc992
Michael Pilquist <mpilquist@gmail.com>
2016-05-24 14:31:01 -0400

    Prep for release against scodec-core 1.10

--- build.sbt
+++ build.sbt
@@ -6,15 +6,17 @@ scodecModule := "scodec-spire"
 scodecPrimaryModule
 scodecPrimaryModuleJvm
 
+crossScalaVersions := crossScalaVersions.value.filter { _.startsWith("2.11.") }
+
 contributors += Contributor("mpilquist", "Michael Pilquist")
 
 rootPackage := "scodec.interop.spire"
 
 libraryDependencies ++= Seq(
-  "org.scodec" %% "scodec-core" % "1.9.0",
+  "org.scodec" %% "scodec-core" % "1.10.0",
   "org.spire-math" %% "spire" % "0.11.0",
-  "org.scalatest" %% "scalatest" % "2.2.0" % "test",
-  "org.scalacheck" %% "scalacheck" % "1.12.0" % "test"
+  "org.scalatest" %% "scalatest" % "3.0.0-M16-SNAP4" % "test",
+  "org.scalacheck" %% "scalacheck" % "1.13.1" % "test"
 )
 
 OsgiKeys.exportPackage := Seq("scodec.interop.spire.*;version=${Bundle-Version}")
--- src/test/scala/scodec/interop/spire/CodecSuite.scala
+++ src/test/scala/scodec/interop/spire/CodecSuite.scala
@@ -5,7 +5,7 @@ package spire
 import scala.collection.GenTraversable
 
 import org.scalacheck.{ Arbitrary, Gen }
-import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.{ Assertion, Matchers, WordSpec }
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 
 import shapeless.Lazy
@@ -14,11 +14,11 @@ import scodec.bits.BitVector
 
 abstract class CodecSuite extends WordSpec with Matchers with GeneratorDrivenPropertyChecks {
 
-  protected def roundtrip[A](a: A)(implicit c: Lazy[Codec[A]]): Unit = {
+  protected def roundtrip[A](a: A)(implicit c: Lazy[Codec[A]]): Assertion = {
     roundtrip(Codec[A], a)
   }
 
-  protected def roundtrip[A](codec: Codec[A], a: A): Unit = {
+  protected def roundtrip[A](codec: Codec[A], a: A): Assertion = {
     val encoded = codec.encode(a)
     val Attempt.Successful(DecodeResult(decoded, remainder)) = codec.decode(encoded.toOption.get)
     remainder shouldEqual BitVector.empty
